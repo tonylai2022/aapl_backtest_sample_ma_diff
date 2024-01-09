24cce14146a2cb734c21d8c4f51ac28fe2926f0c
tonylai2022 <98617290+tonylai2022@users.noreply.github.com>
2024-01-09 13:05:31 -0800

    Update backtester.py

--- facade/backtester.py
+++ facade/backtester.py
@@ -24,38 +24,6 @@ class Backtester:
         self.backtest_result_df = None
         self.opt_df = None
         
-    # def backtest_breakout_signal(self, upper_bound: float, lower_bound: float,
-    #                               unit_tc: float=0, is_long_exceeding_upper: bool=True, 
-    #                               cache_df: bool=False, annualized_factor: int=365) -> \
-    #     Dict[str, Optional[float]]:
-    #     # backtest
-    #     df = self._signal_price_df
-    #     df['benchmark'] = df['return'].cumsum()
-    #     if is_long_exceeding_upper:
-    #         df['position'] = np.where(df['signal'] > upper_bound, 1, np.where(df['signal'] < lower_bound, -1, 0))
-    #     else:
-    #         df['position'] = np.where(df['signal'] > upper_bound, -1, np.where(df['signal'] < lower_bound, 1, 0))
-    #     df['transaction_cost'] = abs(df['position']-df['position'].shift(1)) * unit_tc
-    #     df['pnl'] = df['position'].shift(1) * df['return'] - df['transaction_cost']
-    #     df['cumulative_pnl'] = df['pnl'].cumsum()
-
-    #     # cache
-    #     if cache_df:
-    #         self.backtest_result_df = df
-
-    #     # performance evaluation
-    #     sharpe_ratio: Optional[float] = util_performance.compute_sharpe_ratio(df['pnl'], annualized_factor)
-    #     calmar_ratio: Optional[float] = util_performance.compute_calmar_ratio(df['pnl'], annualized_factor)
-    #     annualized_return: float = util_performance.compute_annualized_return(df['pnl'], annualized_factor)
-    #     maximum_drawdown: float = util_performance.compute_maximum_drawdown(df['pnl'])
-    #     trading_number: float = abs(df['position']).sum()
-    #     result_dict: Dict[str, Optional[float]] = {
-    #         'sharpe_ratio': sharpe_ratio, 'calmar_ratio': calmar_ratio, 
-    #         'annualized_return': annualized_return, 'maximum_drawdown': maximum_drawdown, 
-    #         'trading_number': trading_number}
-    #     return result_dict
-    
-        
     def backtest_momentum_ma_diff(self, rolling, pct_diff_thres, unit_tc=0, is_momentum=False, is_long_only=False,
                                   is_short_only=False, symmetric=False, annualized_factor=252, train_test_mode='all',
                                   cache_df=False):
@@ -110,33 +78,6 @@ class Backtester:
             'annualized_return': annualized_return, 'maximum_drawdown': maximum_drawdown,
             'trading_number': trading_number}
         return result_dict
-                
-    
-    
-    # def create_optimization_df(self, backtest_logic_name: str, upper_param_range: List[float], 
-    #                            lower_param_range: List[float], unit_tc: float, is_long_exceeding_upper: bool=True) -> \
-    #     pd.DataFrame:
-    #     # init
-    #     result_list: List[Dict[str, Optional[float]]] = []
-    #     upper_param_start: float = upper_param_range[0]
-    #     upper_param_end: float = upper_param_range[1]
-    #     lower_param_start: float = lower_param_range[0]
-    #     lower_param_end: float = lower_param_range[1]
-    #     upper_param_step: float = (upper_param_end - upper_param_start)/ 20
-    #     lower_param_step: float = (lower_param_end - lower_param_start)/ 20
-        
-    #     for upper_param in np.arange(upper_param_start, upper_param_end, upper_param_step):
-    #         for lower_param in np.arange(lower_param_start, lower_param_end, lower_param_step):
-    #             if backtest_logic_name == 'breakout_signal':
-    #                 result_dict: Dict[str, Optional[float]] = self.backtest_breakout_signal(
-    #                     upper_param, lower_param, unit_tc, is_long_exceeding_upper)
-    #                 result_dict['upper_param'] = upper_param
-    #                 result_dict['lower_param'] = lower_param
-    #                 result_list.append(result_dict)
-    #     # optimise result
-    #     result_df = pd.DataFrame(result_list)   
-    #     self.opt_df = result_df 
-    #     return result_df
     
     def create_optimization_df(self, backtest_logic_name: str, rolling_range: List[int], pct_diff_thres_range: List[float],
                                unit_tc: float, is_momentum=False, is_long_only=False, is_short_only=False,
@@ -167,13 +108,6 @@ class Backtester:
         opt_df_sorted = opt_df_sorted[opt_df_sorted['trading_number'] >= min_trading_num]
     
         return opt_df_sorted
-    
-    # def plot_param_heatmap(self) -> None:
-    #     if self.opt_df is None:
-    #         logger.warning('Optimization result df is None!')
-    #         return
-    #     param_heatmap_table = self.opt_df.pivot(index='upper_param', columns='lower_param', values='sharpe_ratio')
-    #     sn.heatmap(param_heatmap_table, annot=True)
         
     def plot_param_heatmap(self) -> None:
         if self.opt_df is None:
